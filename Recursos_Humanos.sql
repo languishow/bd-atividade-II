848c6dcb204606a53b73535db8a3426c2fb084d1
languishow <134288755+languishow@users.noreply.github.com>
2023-09-08 20:25:30 -0300

    Add files via upload

---
+++ Recursos_Humanos.sql
@@ -0,0 +1,66 @@
+-- Criar tabela
+CREATE DATABASE Recursos_Humanos;
+
+-- Mostrar Tabela
+SHOW DATABASES;
+
+-- Acessar Banco de Dados
+USE Recursos_Humanos;
+
+CREATE TABLE Recursos_Humanos.Departamentos(
+ID int(30) auto_increment not null primary key,
+NomeDepartamento varchar(255) not null
+);
+
+CREATE TABLE Recursos_Humanos.Funcionarios(
+ID int(30) auto_increment not null primary key,
+Id_Departamento int,
+Nome varchar (255),
+Cargo varchar (255),
+Salario decimal (10,2),
+foreign key (id_departamento) references departamentos(ID)
+);
+
+-- Inserindo dados dos departamentos
+
+INSERT INTO Recursos_Humanos.Departamentos(NomeDepartamento)
+VALUES('Financeiro');
+
+INSERT INTO Recursos_Humanos.Departamentos(NomeDepartamento)
+VALUES('Adiministativo');
+
+INSERT INTO Recursos_Humanos.Departamentos(NomeDepartamento)
+VALUES('Juridico');
+
+INSERT INTO Recursos_Humanos.Departamentos(NomeDepartamento)
+VALUES('Pessoal');
+
+INSERT INTO Recursos_Humanos.Departamentos(NomeDepartamento)
+VALUES('Comercial');
+
+-- Inserindo dados dos funcionarios
+
+INSERT INTO Recursos_Humanos.Funcionarios(ID_Departamento,Nome,Cargo,Salario)
+VALUES( 1 ,'Gabriel','Programador', 1500.00);
+
+INSERT INTO Recursos_Humanos.Funcionarios(ID_Departamento,Nome,Cargo,Salario)
+VALUES( 2 ,'Fernando','Artista', 1000.00);
+
+INSERT INTO Recursos_Humanos.Funcionarios(ID_Departamento,Nome,Cargo,Salario)
+VALUES( 3 ,'Caio','Jovem Aprendiz', 500.00);
+
+INSERT INTO Recursos_Humanos.Funcionarios(ID_Departamento,Nome,Cargo,Salario)
+VALUES( 4 ,'Venicio','Comerciante', 950.00);
+
+INSERT INTO Recursos_Humanos.Funcionarios(ID_Departamento,Nome,Cargo,Salario)
+VALUES( 5 ,'Raphael','Dono de Depositos', 750.00);
+
+
+
+
+
+
+
+
+
+Select * From Recursos_Humanos.Funcionarios;
